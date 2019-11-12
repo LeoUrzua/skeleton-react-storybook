af076486347f7a439a993bbc5e303475060522a8
Leonardo Urzua <d.leonardo.urzua@gmail.com>
2019-11-12 15:23:14 -0600

    feat: new feat

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App: React.FC = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Publish a new version
         </a>
       </header>
     </div>
